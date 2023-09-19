fa95154450ad9518e06a3a89a99f3b52a4136c29
Samatqyzy <zhuldyznamazbayeva@gmail.com>
2023-09-19 15:38:28 +0600

    fix(all): minor mistakes

---
+++ pages/services/index.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="services">
+    <div class="container-1">
+      <UiBreadcrumbs :links="links" />
+      <UiHeading class="services__title">Услуги</UiHeading>
+      <div class="services__subtitle">Какое-то долгое описание услуг. Прям очень долгое описание услуг. Сами услуги классные</div>
+      <div class="services__container">
+        <div 
+          class="services__service" 
+          v-for="service in services" 
+          :key="service.id"
+          @click="$router.push(`/services/${service.id}`)"
+        >
+          <img :src="service.image" alt="service image" />
+          <div class="service__overlay"></div>
+          <div class="service__text">{{ service.name }}</div>
+        </div>
+      </div>
+    </div>
+    <SharedTickets />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      services: []
+    }
+  },
+  async fetch() {
+    await this.getServices()
+  },
+  methods: {
+    async getServices() {
+      this.services = await this.$axios.$get('/services/')
+    }
+  },
+  computed: {
+    links() {
+      return [
+        {
+          title: 'Главная',
+          url: '/'
+        },
+        {
+          title: 'Услуги',
+          url: '/services'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.services {
+  background-color: #F8FAFB;
+
+  &__title {
+    margin-bottom: 30px;
+  }
+
+  &__subtitle {
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px;
+    margin-bottom: 30px;
+  }
+
+  &__container {
+    display: grid;
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+    column-gap: 25px;
+    row-gap: 30px;
+    padding-bottom: 80px;
+  }
+
+  &__service {
+    height: 470px;
+    border-radius: 10px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      border-radius: 10px;
+    }
+  }
+}
+
+.service {
+  &__overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    background: linear-gradient(1deg, rgba(0, 0, 0, 0.54) -16.13%, rgba(255, 255, 255, 0.00) 75.69%);
+  }
+
+  &__text {
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 26px;
+    position: absolute;
+    bottom: 30px;
+    left: 31px;
+    z-index: 2;
+    color: #FFF;
+  }
+}
+</style>
