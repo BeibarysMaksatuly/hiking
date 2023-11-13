ca41dacbb8420a9b91e93b91525cc6b5b988d848
BeibarysMaksatuly <beibarys.m.03@gmail.com>
2023-11-13 15:36:26 +0600

    fixes

--- components/about/security.vue
+++ components/about/security.vue
@@ -8,34 +8,44 @@
       <div class="security__div">
         <div class="security__row">
           <div class="security__number">{{ $t("about.securityNumber") }}</div>
-          <div class="security__descr">{{ $t("about.securityDescription") }}</div>
+          <div class="security__descr">
+            {{ $t("about.securityDescription") }}
+          </div>
         </div>
         <div class="security__row-1">{{ $t("about.securityRow1") }}</div>
       </div>
       <div class="security__div">
         <div class="security__row">
           <div class="security__number">{{ $t("about.securityNumber1") }}</div>
-          <div class="security__descr">{{ $t("about.securityDescription1") }}</div>
+          <div class="security__descr">
+            {{ $t("about.securityDescription1") }}
+          </div>
         </div>
-          <div class="security__row-1">{{ $t("about.securityDescription2") }}</div>
+        <div class="security__row-1">
+          {{ $t("about.securityDescription2") }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
 .security {
-  background-color: #fff;
-  padding-top: 90px;
-  padding-bottom: 90px;
+  width: 100%;
+
   display: flex;
   flex-direction: row;
   align-items: center;
+
+  background-color: #fff;
+  padding-top: 90px;
+  padding-bottom: 90px;
   gap: 15px;
   @include phone {
     flex-direction: column;
     gap: 70px;
     padding-top: 80px;
     padding-bottom: 80px;
+    align-items: flex-start;
   }
 
   &__cont {
@@ -44,45 +54,6 @@
     gap: 40px;
     flex: 1;
   }
-  &__cont-1 {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    width: 100%;
-    flex: 1;
-
-  }
-
-  &__row {
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 20px;
-
-    @include phone {
-      flex-direction: column;
-      align-items: flex-start;
-    }
-  }
-
-  &__row-1 {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: flex-start;
-    width: 382px;
-    color:  #7D92A1;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: normal;
-    @include phone {
-      width: 100%;
-    }
-
-  }
-
   &__subtitle {
     font-size: 16px;
     font-style: normal;
@@ -97,23 +68,51 @@
     line-height: normal;
   }
 
+  &__cont-1 {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    width: 100%;
+    flex: 1;
+  }
   &__div {
     display: flex;
-    padding: 20px;
     flex-direction: column;
-    justify-content: center;
     align-items: flex-end;
+
+    padding: 20px;
     gap: 15px;
-    align-self: stretch;
     border-radius: 5px;
-    background-color: #F6F8FA;
+    background-color: #f6f8fa;
     @include phone {
       align-items: flex-start;
     }
   }
+  &__row {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 20px;
+
+    @include phone {
+      flex-direction: column;
+      align-items: flex-start;
+    }
+  }
+  &__row-1 {
+    width: 382px;
+    color: #7d92a1;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: normal;
+    @include phone {
+      width: 100%;
+    }
+  }
 
   &__number {
-    color: #0069B4;
+    color: #0069b4;
     text-align: right;
     font-size: 50px;
     font-style: normal;
