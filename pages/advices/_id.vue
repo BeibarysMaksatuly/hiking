b0a472b8e3fda9a9f77a12393b650d313af998a3
BeibarysMaksatuly <beibarys.m.03@gmail.com>
2023-11-15 00:52:23 +0600

    fixes

--- pages/advices/_id.vue
+++ pages/advices/_id.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="advice">
-    <pre>{{ category }}</pre>
     <UiBreadcrumbs :links="links" class="container-1" />
     <div class="advice__hero">
       <img :src="category.detail_image" alt="category image" />
+      <p class="container-1">{{ category.inside_text }}</p>
     </div>
     <div class="container-1">
       <UiHeading class="advice__title">{{ $t("header.advices") }}</UiHeading>
       <client-only>
-        <swiper
-          :pagination="true"
-          class="adviceSwiper"
-          :options="swiperOptions"
-        >
+        <swiper :pagination="true" :options="swiperOptions">
           <swiper-slide
             v-for="file in category.files"
             :key="file.id"
@@ -29,20 +25,7 @@
         v-if="otherCategories && otherCategories.length"
       >
         <div class="category__title">{{ $t("advices.otherAdvices") }}</div>
-        <div class="category__data">
-          <div
-            class="category__info"
-            v-for="info in otherCategories"
-            :key="info.id"
-            @click="$router.push(localePath(`/advices/${info.id}`))"
-          >
-            <img :src="info.image" alt="info image" class="info__image" />
-            <div class="info__text">
-              <div class="info__title">{{ info.title }}</div>
-              <div class="info__subtitle" v-html="info.text"></div>
-            </div>
-          </div>
-        </div>
+        <CategoryList :recommendations="otherCategories" />
       </div>
     </div>
     <SharedTickets />
@@ -57,6 +40,7 @@
     </v-overlay>
   </div>
 </template>
+
 <script>
 import Swiper, { Navigation, Pagination } from "swiper";
 Swiper.use([Navigation, Pagination]);
@@ -107,11 +91,12 @@ export default {
   },
 };
 </script>
+
 <style lang="scss" scoped>
 .advice {
   background-color: #f8fafb;
-
   &__hero {
+    position: relative;
     width: 100%;
     height: 400px;
     margin-bottom: 80px;
@@ -124,14 +109,32 @@ export default {
       height: 100%;
       object-fit: cover;
     }
+    p {
+      position: absolute;
+      z-index: 1;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+
+      color: #fff;
+      text-align: center;
+      font-size: 60px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: normal;
+      @include phone {
+        font-size: 30px;
+      }
+    }
   }
+
   &__title {
     margin-bottom: 40px;
   }
 
   &__photo {
     width: 100%;
-    height: 171px;
+    height: 300px;
     border-radius: 10px;
     @include phone {
       height: 150px;
@@ -145,18 +148,29 @@ export default {
   }
 
   &__text {
-    font-size: 20px;
-    font-style: normal;
-    line-height: 24px;
+    white-space: pre-line;
     margin-top: 40px;
-    padding-bottom: 160px;
+    margin-bottom: 160px;
     @include phone {
-      padding-bottom: 80px;
+      margin-bottom: 80px;
     }
   }
 
   &__other {
     padding-bottom: 80px;
+  }
+}
+
+.category__title {
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 120%; /* 33.6px */
+  letter-spacing: 0.084px;
+  margin-bottom: 40px;
+  @include phone {
+    font-size: 20px;
+    letter-spacing: 0.06px;
   }
 }
 
@@ -173,94 +187,5 @@ export default {
 :deep(.swiper-pagination-bullet) {
   background: $c-white;
   opacity: 1;
-}
-
-.category {
-  &__title {
-    font-size: 28px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 120%; /* 33.6px */
-    letter-spacing: 0.084px;
-    margin-bottom: 40px;
-    @include phone {
-      font-size: 20px;
-      font-style: normal;
-      font-weight: 700;
-      line-height: 120%; /* 24px */
-      letter-spacing: 0.06px;
-    }
-  }
-  &__data {
-    display: grid;
-    grid-template-columns: repeat(3, minmax(0, 1fr));
-    column-gap: 20px;
-    row-gap: 30px;
-    @include phone {
-      grid-template-columns: repeat(2, minmax(0, 1fr));
-    }
-  }
-
-  &__info {
-    border-radius: 10px;
-    background-color: #fff;
-    height: 376px;
-    border-radius: 15px 15px 0px 0px;
-    @include phone {
-      height: 305px;
-    }
-  }
-}
-
-.info {
-  &__image {
-    width: 100%;
-    height: 220px;
-    object-fit: cover;
-    border-radius: 15px 15px 0px 0px;
-    @include phone {
-      height: 154px;
-    }
-  }
-
-  &__text {
-    display: flex;
-    flex-direction: column;
-    padding: 20px;
-    margin-bottom: 2px;
-    @include phone {
-      padding: 10px;
-    }
-  }
-  &__title {
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 24px;
-    margin-bottom: 15px;
-    @include phone {
-      font-size: 14px;
-      font-style: normal;
-      font-weight: 600;
-      line-height: 130%;
-    }
-  }
-  &__subtitle {
-    height: 72px;
-    overflow: hidden;
-    word-wrap: normal;
-    text-overflow: ellipsis;
-
-    &::after {
-      content: "...";
-    }
-    @include phone {
-      height: 90px;
-      font-size: 12px;
-      font-style: normal;
-      font-weight: 400;
-      line-height: 15px;
-    }
-  }
 }
 </style>
--- pages/advices/index.vue
+++ pages/advices/index.vue
@@ -10,20 +10,7 @@
           :key="category.id"
         >
           <div class="category__title">{{ category.title }}</div>
-          <div class="category__data">
-            <div
-              class="category__info"
-              v-for="info in category.recommendations"
-              :key="info.id"
-              @click="$router.push(localePath(`/advices/${info.id}`))"
-            >
-              <img :src="info.image" alt="info image" class="info__image" />
-              <div class="info__text">
-                <div class="info__title">{{ info.title }}</div>
-                <div class="info__subtitle">{{ info.description }}</div>
-              </div>
-            </div>
-          </div>
+          <CategoryList :recommendations="category.recommendations" />
         </div>
       </div>
     </div>
@@ -98,89 +85,15 @@ export default {
   }
 }
 
-.category {
-  &__title {
-    font-size: 28px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 120%; /* 33.6px */
-    letter-spacing: 0.084px;
-    @include phone {
-      font-size: 20px;
-      letter-spacing: 0.06px;
-    }
-  }
-  &__data {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    column-gap: 20px;
-    row-gap: 30px;
-    @include phone {
-      grid-template-columns: repeat(2, 1fr);
-    }
-    @media (max-width: 430px) {
-      grid-template-columns: repeat(1, 1fr);
-    }
-  }
-
-  &__info {
-    display: flex;
-    flex-direction: column;
-    border-radius: 10px;
-    background-color: #fff;
-    height: 376px;
-		cursor: pointer;
-    @include phone {
-      height: 305px;
-    }
-  }
-}
-
-.info {
-  &__image {
-    width: 100%;
-    height: 220px;
-    object-fit: cover;
-    border-radius: 10px 10px 0px 0px;
-    @include phone {
-      height: 154px;
-    }
-  }
-
-  &__text {
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-    padding: 20px 20px 25px;
-    @include phone {
-      padding: 10px;
-    }
-  }
-  &__title {
+.category__title {
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 120%; /* 33.6px */
+  letter-spacing: 0.084px;
+  @include phone {
     font-size: 20px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 24px;
-    @include phone {
-      font-size: 14px;
-      line-height: 130%;
-    }
-  }
-  &__subtitle {
-    font-size: 18px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 24px;
-
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 3;
-    overflow: hidden;
-    @include phone {
-      font-size: 12px;
-      line-height: 15px;
-      -webkit-line-clamp: 5;
-    }
+    letter-spacing: 0.06px;
   }
 }
 </style>
